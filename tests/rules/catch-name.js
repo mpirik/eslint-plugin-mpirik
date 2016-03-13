c7619fd8a65a5aa8a1dc75b262a93ac50815c880
Jim Geurts <jim@biacreations.com>
2016-03-12 19:08:07 -0600

    Code formatting

--- tests/rules/catch-name.js
+++ tests/rules/catch-name.js
@@ -4,24 +4,24 @@ var RuleTester = require("eslint/lib/testers/rule-tester");
 var ruleTester = new RuleTester();
 ruleTester.run("catch-name", rule, {
   valid: [
-    "try {} catch(ex) {}",
+    "try {} catch (ex) {}",
     {
-      code: "try {} catch(exception) {}",
+      code: "try {} catch (exception) {}",
       options: ["^(ex|exception)$"]
     },
     {
-      code: "try {} catch(funk) {}",
+      code: "try {} catch (funk) {}",
       options: ["funk"]
     }
   ],
   invalid: [{
-    code: "try {} catch(funk) {}",
+    code: "try {} catch (funk) {}",
     errors: [{
       message: "Invalid variable name for catch block parameter.",
       type: "CatchClause"
     }]
   }, {
-    code: "try {} catch(except) {}",
+    code: "try {} catch (except) {}",
     errors: [{
       message: "Invalid variable name for catch block parameter.",
       type: "CatchClause"
