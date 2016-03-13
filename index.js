5783ac35891a6a2843a6c712efac7be5e9fcd827
Jim Geurts <jim@biacreations.com>
2016-03-12 21:38:51 -0600

    Define trycatch rules

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 module.exports = {
-  extends: [
-    'eslint-config-airbnb/legacy'
-  ].map(require.resolve),
   rules: {
     'array-func-names': require('./rules/array-func-names.js'),
+    'array-func-trycatch': require('./rules/array-func-trycatch.js'),
+    'asyncjs-func-trycatch': require('./rules/asyncjs-func-trycatch.js'),
     'catch-name': require('./rules/catch-name.js')
   },
   configs: {
     recommended: {
       rules: {
         'mpirik/array-func-names': 2,
-        'mpirik/catch-name': 2,
         'mpirik/array-func-trycatch': 2,
-        'mpirik/asyncjs-func-trycatch': 2
+        'mpirik/asyncjs-func-trycatch': 2,
+        'mpirik/catch-name': 2
       }
     }
   }
